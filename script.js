0d58b6cd42d98998265e2d9cdd4d357c3a26642a
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-06 11:30:01 +0100

    Add getComputerChoice() func

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+function getComputerChoice() {
+let choice;
+   let random = Math.floor(Math.random()*3)+1
+   if(random === 1){
+        choice="rock";
+   }else if(random === 2){
+        choice = "paper";
+   }else{
+        choice="scissors";
+   }
+   return choice;
+}
+
+getComputerChoice();
