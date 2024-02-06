9ad60c89f6f9037a693fc7d6d274df2f67ce8bc9
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-06 11:54:32 +0100

    Finished the game

--- script.js
+++ script.js
@@ -10,5 +10,50 @@ let choice;
    }
    return choice;
 }
+function playRound(playerChoice, computerChoice,score,score_ai){
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerChoice){
+        console.log(`Tie! Player choice: ${playerChoice} and Computer choice: ${computerChoice} are equal.`)
+        return "Tie"
+    }
+    //win conditions
+    else if((playerChoice === "paper" && computerChoice ==="rock")|| 
+        (playerChoice === "rock" && computerChoice ==="scissors") ||
+        (playerChoice === "scissors" && computerChoice ==="paper"))
+        {
+        console.log(`Player win! ${playerChoice} beat ${computerChoice}`)
+        return "Win";
+        }
+    //lose condition
+    else{
+        console.log(`Computer win! ${computerChoice} beat ${playerChoice}`)
+        return "Lose";
+        }
+}
 
-getComputerChoice();
+function playGame(){
+    //var
+    let score = 0;
+    let score_ai = 0;
+
+
+    for(let i = 0; i < 5;i++){
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        round = playRound(playerSelection,computerSelection,score);
+        if (round === "Win"){
+            score++;
+        }else if(round === "Lose"){
+            score_ai++;
+        }
+    }
+    if(score> score_ai){
+        console.log("You win the game!")
+    }else if(score<score_ai){
+        console.log("Youn lose the game!")
+    }else{
+        console.log("The is draw!")
+    }
+}
+
+playGame();
